fb7250e219baa87c7bb6cff02d5f230acf4c0256
Hanisha <hanishareddyv@gmail.com>
2021-08-07 20:00:06 +0530

    miniproject

---
+++ src/main/java/package/applicationApplication.java
@@ -0,0 +1,13 @@
+package package;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class applicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(applicationApplication.class, args);
+    }
+
+}
